32ecdcdeeeaab820245bce1f667225daebe2ac09
Utkarsh Rai <utkarsh.rai60@gmial.com>
2019-09-05 09:28:00 +0530

    line drawing pattern added

--- gui.c
+++ gui.c
@@ -19,26 +19,61 @@ void gui_draw_point(uint16_t xStart,uint16_t yStart,uint16_t color)
   lcd_WR_data_16bit(color);
 }
 
-void gui_draw_line(uint16_t xStart,uint16_t yStart,uint16_t xEnd,uint16_t yEnd)
+void gui_draw_line(uint16_t xStart,uint16_t yStart,uint16_t xEnd,uint16_t yEnd,uint16_t color)
 {
     lcd_set_cursor(xStart,yStart);
-    uint16_t x_delta=xEnd-xStart;
-    uint16_t y_delta=yEnd-yStart;
-      if(y_delta>0&&x_delta>0)
+    uint16_t xDelta=xEnd-xStart;
+    uint16_t yDelta=yEnd-yStart;
+      if(yDelta>0&&xDelta>0)
       {
-        /*first quadrant */
+         uint16_t xPos=xStart;
+         uint16_t yPos=yStart;
+         while(xPos!=xEnd && yPos!=yEnd)
+         {
+           xPos += xDelta;
+           yPos -= yDelta;
+            
+           gui_draw_point(xPos,yPos,color);
+         }       
       }   
-      else if(y_delta<0&&x_delta>0)
+      else if(yDelta<0&&xDelta>0)
       {
         /*Second quadrant */
+         uint16_t xPos=xStart;
+         uint16_t yPos=yStart;
+         while(xPos!=xEnd && yPos!=yEnd)
+         {
+           xPos -= xDelta;
+           yPos -= yDelta;
+            
+           gui_draw_point(xPos,yPos,color);
+         }
       }
-      else if(y_delta<0&&x_delta<0)
+      else if(yDelta<0&&xDelta<0)
       {
         /*Third qaudrant */
+      uint16_t xPos=xStart;
+         uint16_t yPos=yStart;
+         while(xPos!=xEnd && yPos!=yEnd)
+         {
+           xPos -= xDelta;
+           yPos += yDelta;
+            
+           gui_draw_point(xPos,yPos,color);
+         }
       }
-      else if(y_delta>0&& x_delta<0)
+      else if(yDelta>0&& xDelta<0)
       {
         /*Fourth quadrant */
+        uint16_t xPos=xStart;
+         uint16_t yPos=yStart;
+         while(xPos!=xEnd && yPos!=yEnd)
+         {
+           xPos += xDelta;
+           yPos += yDelta;
+            
+           gui_draw_point(xPos,yPos,color);
+         }
       }
 }
 
